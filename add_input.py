f02b4bae0c6c0b19efc3a2edd17f66b383bc532e
AnnaKhamaganova <annaproxorixina@gmail.com>
2025-01-20 19:12:44 +0700

    Grade 1. Этап 1: Задание 3

--- add_input.py
+++ add_input.py
@@ -5,7 +5,7 @@ import datetime as DT
 # Просим пользователя ввести данные
 
 username = input('Введите имя пользователя: ')
-title = input('Введите название заметки: ')
+title = input('Введите заголовок заметки: ')
 content = input('Введите описание заметки: ')
 status = input('Введите статус заметки: ')
 temp_created_date = input('Введите дату создания заметки в формате "день-месяц-год": ')
@@ -15,3 +15,12 @@ temp_issue_date = input('Введите дату истечения заметк
 
 created_date = DT.datetime.strptime(temp_created_date, '%d-%m-%Y').date()
 issue_date = DT.datetime.strptime(temp_issue_date, '%d-%m-%Y').date()
+
+# Выводим данные
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', f"{created_date:%d-%m}")
+print('Дата истечения заметки (дедлайн):', f"{issue_date:%d-%m}")
